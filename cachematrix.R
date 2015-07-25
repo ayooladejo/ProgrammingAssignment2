9a8621ec50e22130d281550359f94cce8e0f4797
ayooladejo <ayooladejo@gmail.com>
2015-07-25 19:50:47 +0200

    lets do this

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# makeCacheMatrix - This function creates a special "matrix" object that can cache its inverse.
+# 1. Sets the value of the matrix
+# 2. Gets the value of the matrix
+# 3. Sets the value of inverse of the matrix
+# 4. Gets the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  d<-NULL
+  set<-function(y){
+    x<<-y
+    d<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) d<<- solve
+  getmatrix<-function() d
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# This function computes the inverse of the special "matrix" returned by makeCacheMartix above
+cacheSolve <- function(x=matrix(), ...) {
+  d<-x$getmatrix()
+  if(!is.null(d)){
+    message("obtaining cached data")
+    return(d)
+  }
+  matrix<-x$get()
+  d<-solve(matrix, ...)
+  x$setmatrix(d)
+  d
 }
+#Sample Run
+
+#x = rbind(c(2, -1/2), c(-1/2, 2))
+#> d = makeCacheMatrix(x)
+#> d$get()
+#     [,1] [,2]
+#[1,]  2.0 -0.5
+#[2,] -0.5  2.0
+#> cacheSolve(d)
+#          [,1]      [,2]
+#[1,] 0.5333333 0.1333333
+#[2,] 0.1333333 0.5333333
+#> cacheSolve(d)
+#obtaining cached data
+#          [,1]      [,2]
+#[1,] 0.5333333 0.1333333
+#[2,] 0.1333333 0.5333333
